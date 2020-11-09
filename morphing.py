46eeb57788d0699ae2bc7df787155802decdc805
sugyan <sugi1982@gmail.com>
2020-11-09 23:34:21 +0900

    Update morphing script

--- morphing.py
+++ morphing.py
@@ -6,29 +6,23 @@ from datetime import datetime
 from PIL import Image
 
 
-def run(model_path, outdir):
+def run(model_path, num_images, outdir, seed, steps):
     model = tf.saved_model.load(model_path)
     mapping = model.signatures['mapping']
-    rnd = np.random.RandomState(0)
-    z = rnd.randn(3, 512)
+    rnd = np.random.RandomState(seed)
+    z = rnd.randn(num_images, 512)
     z = mapping(latents=tf.constant(z, tf.float32))['dlatents'].numpy()
 
-    step = 30
     inputs = []
-    for i in range(step):
-        rate = i / step
-        inputs.append(z[0] + (z[1] - z[0]) * rate)
-    for i in range(step):
-        rate = i / step
-        inputs.append(z[1] + (z[2] - z[1]) * rate)
-    for i in range(step):
-        rate = i / step
-        inputs.append(z[2] + (z[0] - z[2]) * rate)
+    for i in range(num_images):
+        for j in range(steps):
+            rate = j / steps
+            inputs.append(z[i] + (z[(i + 1) % num_images] - z[i]) * rate)
 
     synthesis = model.signatures['synthesis']
     for i, latents_in in enumerate(inputs):
         now = datetime.now()
-        out_path = os.path.join(outdir, f'{i:03d}.png')
+        out_path = os.path.join(outdir, f'morphing_{i:03d}.png')
         images = synthesis(dlatents=tf.constant([latents_in], tf.float32))['images']
         img = Image.fromarray(images.numpy()[0])
         img.save(out_path)
@@ -39,7 +33,10 @@ def run(model_path, outdir):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('model_path', type=str)
+    parser.add_argument('--num', type=int, default=3)
+    parser.add_argument('--steps', type=int, default=10)
+    parser.add_argument('--seed', type=int, default=0)
     parser.add_argument('--outdir', type=str, default='out')
     args = parser.parse_args()
 
-    run(args.model_path, args.outdir)
+    run(args.model_path, args.num, args.outdir, args.seed, args.steps)
