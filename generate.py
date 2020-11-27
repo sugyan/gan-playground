da9a38f94468d24e3cf0e24fdb592b536c62fd83
sugyan <sugi1982@gmail.com>
2020-11-28 00:14:20 +0900

    Update generate script

--- generate.py
+++ generate.py
@@ -13,20 +13,20 @@ def run(model_path, num_images, outdir, seed):
     zs = rnd.randn(num_images, 1, 512)
 
     for i, z in enumerate(zs):
-        out_path = os.path.join(outdir, f'generate_{i:03d}.png')
+        out_path = os.path.join(outdir, f"generate_{i:05d}.png")
         now = datetime.now()
-        images = generate(latents=tf.constant(z, tf.float32))['images']
+        images = generate(latents=tf.constant(z, tf.float32))["images"]
         Image.fromarray(images.numpy()[0]).save(out_path)
         elapsed = datetime.now() - now
-        print(f'{out_path} ({elapsed})')
+        print(f"{out_path} ({elapsed})")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('model_path', type=str)
-    parser.add_argument('--num', type=int, default=1)
-    parser.add_argument('--seed', type=int, default=0)
-    parser.add_argument('--outdir', type=str, default='out')
+    parser.add_argument("model_path", type=str)
+    parser.add_argument("--num", type=int, default=1)
+    parser.add_argument("--seed", type=int, default=None)
+    parser.add_argument("--outdir", type=str, default="out")
     args = parser.parse_args()
 
     run(args.model_path, args.num, args.outdir, args.seed)
