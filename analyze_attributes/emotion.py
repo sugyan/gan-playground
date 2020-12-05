3a18bb3a6fc8d12b81e5084e9d14f0748d305dea
sugyan <sugi1982@gmail.com>
2020-12-05 23:27:41 +0900

    Update emotion.py

--- analyze_attributes/emotion.py
+++ analyze_attributes/emotion.py
@@ -1,12 +1,14 @@
 import argparse
 import glob
 import os
+import dlib
 import numpy as np
 import pandas as pd
 import paz.processors as pr
+from paz.abstract import Box2D
 from paz.backend.image import load_image
 from paz.datasets.utils import get_class_names
-from paz.applications import HaarCascadeFrontalFace, MiniXceptionFER
+from paz.applications import MiniXceptionFER
 
 emotions = get_class_names("FER")
 
@@ -14,15 +16,27 @@ emotions = get_class_names("FER")
 class EmotionDetector(pr.Processor):
     def __init__(self):
         super(EmotionDetector, self).__init__()
-        self.detect = HaarCascadeFrontalFace(draw=False)
+        self.detector = dlib.get_frontal_face_detector()
         self.crop = pr.CropBoxes2D()
         self.classify = MiniXceptionFER()
 
     def call(self, image):
-        boxes2D = self.detect(image)["boxes2D"]
-        cropped_images = self.crop(image, boxes2D)
+        detections, scores, _ = self.detector.run(image, 1)
+        boxes2D = []
+        for detection, score in zip(detections, scores):
+            boxes2D.append(
+                Box2D(
+                    [
+                        detection.left(),
+                        detection.top(),
+                        detection.right(),
+                        detection.bottom(),
+                    ],
+                    score,
+                )
+            )
         results = []
-        for cropped_image, box2D in zip(cropped_images, boxes2D):
+        for cropped_image in self.crop(image, boxes2D):
             results.append(self.classify(cropped_image)["scores"])
         return results
 
@@ -46,36 +60,34 @@ def predict(target_dir, data_file):
     df.to_hdf(data_file, key="df")
 
 
-def top_k(df, outdir, k=10):
+def calc_vectors(df, out_file):
     all_dlatents = []
     for index in df.index:
         all_dlatents.append(np.load(f"{index}.npy"))
 
+    k = round(len(df) / 100.0)
+    outputs = {}
     for e in emotions:
-        # TODO
-        if e != "happy":
+        if e == "neutral":
             continue
-
         print(f"top {k} images of {e}")
         dlatents = []
         for index, row in df.sort_values(e, ascending=False)[:k].iterrows():
             print(index, row[e])
             dlatents.append(np.load(f"{index}.npy"))
-        np.save(
-            os.path.join(outdir, f"{e}.npy"),
-            np.mean(dlatents, axis=0) - np.mean(all_dlatents, axis=0),
-        )
+        outputs[e] = np.mean(dlatents, axis=0) - np.mean(all_dlatents, axis=0)
+    np.savez(out_file, **outputs)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("target_dir", type=str)
-    parser.add_argument("--data_file", type=str, default="emotion.h5")
-    parser.add_argument("--outdir", type=str, default="out")
+    parser.add_argument("--data_file", type=str, default="emotions.h5")
+    parser.add_argument("--out_file", type=str, default="emotions.npz")
     args = parser.parse_args()
 
     if not os.path.exists(args.data_file):
         predict(args.target_dir, args.data_file)
 
     df = pd.read_hdf(args.data_file)
-    top_k(df, args.outdir, 30)
+    calc_vectors(df, args.out_file)
