4287cbf1e08c8d5bae91fca6b64b120edee5e4b8
sugyan <sugi1982@gmail.com>
2020-12-09 00:36:05 +0900

    Update headpose.py

--- analyze_attributes/headpose.py
+++ analyze_attributes/headpose.py
@@ -7,11 +7,10 @@ import numpy as np
 import pandas as pd
 
 
-predictor_path = "shape_predictor_68_face_landmarks.dat"
-
-
 class HeadposeDetector:
     def __init__(self):
+        predictor_path = "shape_predictor_68_face_landmarks.dat"
+
         self.model_points = np.array(
             [
                 (0.0, 0.0, 0.0),  # Nose tip
@@ -65,15 +64,37 @@ class HeadposeDetector:
         _, _, _, _, _, _, euler_angles = cv2.decomposeProjectionMatrix(
             cv2.hconcat([rotation_mat, translation_vector])
         )
-        euler_angles = euler_angles.flatten()
-        return euler_angles[1], euler_angles[0], euler_angles[2]
+
+        (nose_end_point2D, jacobian) = cv2.projectPoints(
+            np.array([(0.0, 0.0, 1000.0)]),
+            rotation_vector,
+            translation_vector,
+            camera_matrix,
+            dist_coeffs,
+        )
+        for p in image_points:
+            cv2.circle(image, (int(p[0]), int(p[1])), 3, (0, 0, 255), -1)
+
+        p1 = (int(image_points[0][0]), int(image_points[0][1]))
+        p2 = (int(nose_end_point2D[0][0][0]), int(nose_end_point2D[0][0][1]))
+
+        cv2.line(image, p1, p2, (255, 0, 0), 2)
+
+        # Display image
+        cv2.imshow("Output", image)
+        cv2.waitKey(0)
+
+        return euler_angles.flatten()
 
 
-def predict(target_dir):
+def predict(target_dir, data_file):
     detect = HeadposeDetector()
     indexes = []
     columns = []
     for i, img_file in enumerate(glob.glob(os.path.join(target_dir, "*.png"))):
+        # if not (img_file.endswith("04338.png") or img_file.endswith("00727.png")):
+        # if not (img_file.endswith("04737.png") or img_file.endswith("02743.png")):
+        #     continue
         # Read Image
         im = cv2.imread(img_file)
         yaw, pitch, roll = detect(im)
@@ -81,19 +102,38 @@ def predict(target_dir):
         indexes.append(os.path.abspath(img_file))
         columns.append([yaw, pitch, roll])
 
-        # im = cv2.imread('headPose.jpg', cv2.IMREAD_COLOR)
-        # hoge(im)
-        # break
-
-        if i == 10:
-            break
-    df = pd.DataFrame(columns, index=indexes, columns=["yaw", "pitch", "roll"])
+    df = pd.DataFrame(columns, index=indexes, columns=["pitch", "yaw", "roll"])
     print(df)
+    df.to_hdf(data_file, key="df")
+
+
+def calc_vectors(df, out_file):
+    k = round(len(df) / 100.0)
+    outputs = {}
+    for e in ["pitch", "yaw", "roll"]:
+        print(f"top {k} images of {e}")
+        print("min:")
+        mins = []
+        for index, row in df.sort_values(e, ascending=True)[:k].iterrows():
+            print(index, row[e])
+            mins.append(np.load(f"{index}.npy"))
+        print("max:")
+        maxs = []
+        for index, row in df.sort_values(e, ascending=False)[:k].iterrows():
+            print(index, row[e])
+            maxs.append(np.load(f"{index}.npy"))
+        outputs[e] = np.mean(maxs, axis=0) - np.mean(mins, axis=0)
+    np.savez(out_file, **outputs)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("target_dir", type=str)
+    parser.add_argument("--data_file", type=str, default="headposes.h5")
+    parser.add_argument("--out_file", type=str, default="headposes.npz")
     args = parser.parse_args()
 
-    predict(args.target_dir)
+    if not os.path.exists(args.data_file):
+        predict(args.target_dir, args.data_file)
+    df = pd.read_hdf(args.data_file)
+    calc_vectors(df, args.out_file)
