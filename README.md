70a235c87fb525b1387d6af00a102c8ad1c9ee8d
sugyan <sugi1982@gmail.com>
2021-02-12 01:14:04 +0900

    Update generate.py

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # gan-playground
+
+Using [StyleGAN2 ADA](https://github.com/NVlabs/stylegan2-ada)
+
+
+## Prerequisite
+
+- Python >= 3.x
+- TensorFlow >= 2.4
+
+
+## SavedModel from StyleGAN2-ADA
+
+https://github.com/sugyan/stylegan2-ada/blob/cpu/pkl2savedmodel.py
+
+
+## Generate
+
+```
+python generate.py --savedmodel <Path to SavedModel> --outdir .
+```
--- generate.py
+++ generate.py
@@ -1,13 +1,19 @@
 import argparse
-import os
+import pathlib
 import numpy as np
 import tensorflow as tf
 from datetime import datetime
 from PIL import Image
 
 
-def run(model_path, num_images, outdir, seed, save_dlatents):
-    model = tf.saved_model.load(model_path)
+def run(
+    model_path: pathlib.Path,
+    outdir: pathlib.Path,
+    num_images: int,
+    seed: int,
+    save_dlatents: bool,
+) -> None:
+    model = tf.saved_model.load(str(model_path))
     rnd = np.random.RandomState(seed)
     z = rnd.randn(num_images, 512)
 
@@ -19,24 +25,30 @@ def run(model_path, num_images, outdir, seed, save_dlatents):
     # generate images
     synthesis = model.signatures["synthesis"]
     for i, dlatents_in in enumerate(dlatents):
-        out_path = os.path.join(outdir, f"generate_{i:05d}.png")
+        out_path = outdir.joinpath(f"generate_{i:05d}.png")
+
         now = datetime.now()
         images = synthesis(dlatents=tf.convert_to_tensor([dlatents_in], tf.float32))
         images = images["images"].numpy()
         Image.fromarray(images[0]).save(out_path)
-        elapsed = datetime.now() - now
-        print(f"{out_path} ({elapsed})")
+        print(f"{out_path} ({datetime.now() - now})")
         if save_dlatents:
             np.save(f"{out_path}.npy", dlatents_in)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("model_path", type=str)
+    parser.add_argument("--savedmodel", type=pathlib.Path, required=True)
+    parser.add_argument("--outdir", type=pathlib.Path, required=True)
     parser.add_argument("--num", type=int, default=1)
     parser.add_argument("--seed", type=int, default=None)
-    parser.add_argument("--outdir", type=str, default="out")
     parser.add_argument("--save_dlatents", action="store_true")
     args = parser.parse_args()
 
-    run(args.model_path, args.num, args.outdir, args.seed, args.save_dlatents)
+    run(
+        args.savedmodel.resolve(strict=True),
+        args.outdir.resolve(strict=True),
+        args.num,
+        args.seed,
+        args.save_dlatents,
+    )
