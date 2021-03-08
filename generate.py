a3d2c824677dc7337bf480172b286704fb3fcce7
sugyan <sugi1982@gmail.com>
2021-03-08 23:46:19 +0900

    Fix: Update generate.py for v1 compatibility

--- generate.py
+++ generate.py
@@ -1,25 +1,43 @@
 import argparse
 import pathlib
+from typing import Optional
+
 import numpy as np
 import tensorflow as tf
+from PIL import Image
+
 from generator import ImagesGenerator
+
+
+def main(
+    savedmodel: pathlib.Path,
+    seed: Optional[int],
+    num: int,
+    outdir: pathlib.Path,
+    save_dlatents: bool,
+) -> None:
+    generator = ImagesGenerator(savedmodel, seed, num)
+    for i, (image, dlatents) in enumerate(generator.generate()):
+        out_path = outdir / f"generate_{i:05d}.png"
+        print(out_path)
+        Image.fromarray(image).save(out_path)
+        if save_dlatents:
+            np.save(f"{out_path}.npy", dlatents)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--savedmodel", type=pathlib.Path, required=True)
-    parser.add_argument("--num", type=int, default=1)
     parser.add_argument("--seed", type=int, default=None)
+    parser.add_argument("--num", type=int, default=1)
     parser.add_argument("--outdir", type=pathlib.Path, required=True)
     parser.add_argument("--save_dlatents", action="store_true")
     args = parser.parse_args()
 
-    generator = ImagesGenerator(
-        args.savedmodel.resolve(strict=True), args.seed, args.num
+    main(
+        savedmodel=args.savedmodel.resolve(strict=True),
+        seed=args.seed,
+        num=args.num,
+        outdir=args.outdir.resolve(strict=True),
+        save_dlatents=args.save_dlatents,
     )
-    for i, (image, dlatents) in enumerate(generator.generate()):
-        out_path = str(args.outdir.resolve(strict=True) / f"generate_{i:05d}.png")
-        print(out_path)
-        tf.io.write_file(out_path, tf.compat.v1.image.encode_png(image))
-        if args.save_dlatents:
-            np.save(f"{out_path}.npy", dlatents)
--- generator.py
+++ generator.py
@@ -1,29 +1,80 @@
 import pathlib
+from abc import ABC, abstractmethod
+from typing import Generator, List, Optional, Tuple
+
 import numpy as np
 import tensorflow as tf
-from typing import Generator, List, Optional, Tuple
+
+
+class StyleGAN(ABC):
+    def __init__(self, model_path: pathlib.Path) -> None:
+        self.model = tf.compat.v2.saved_model.load(str(model_path))
+
+    @abstractmethod
+    def mapping(self, latents: np.ndarray) -> np.ndarray:
+        pass
+
+    @abstractmethod
+    def synthesis(self, dlatents: np.ndarray) -> np.ndarray:
+        pass
+
+
+class TF1Generator(StyleGAN):
+    def __init__(self, model_path: pathlib.Path) -> None:
+        self.sess = tf.compat.v1.Session(graph=tf.Graph())
+        with self.sess.graph.as_default():
+            super().__init__(model_path)
+
+            signatures = self.model.signatures
+
+            self.latents_in = tf.compat.v1.placeholder(tf.float32)
+            self.dlatents = signatures["mapping"](self.latents_in)["dlatents"]
+
+            self.dlatents_in = tf.compat.v1.placeholder(tf.float32)
+            self.images = signatures["synthesis"](self.dlatents_in)["images"]
+
+    def mapping(self, latents: np.ndarray) -> np.ndarray:
+        return self.sess.run(self.dlatents, feed_dict={self.latents_in: latents})
+
+    def synthesis(self, dlatents: np.ndarray) -> np.ndarray:
+        return self.sess.run(self.images, feed_dict={self.dlatents_in: [dlatents]})
+
+    def __del__(self) -> None:
+        self.sess.close()
+
+
+class TF2Generator(StyleGAN):
+    def mapping(self, latents: np.ndarray) -> np.ndarray:
+        latents = tf.constant(latents, dtype=tf.float32)
+        return self.model.signatures["mapping"](latents)["dlatents"].numpy()
+
+    def synthesis(self, dlatents: np.ndarray) -> np.ndarray:
+        dlatents = tf.expand_dims(tf.constant(dlatents, dtype=tf.float32), axis=0)
+        return self.model.signatures["synthesis"](dlatents)["images"].numpy()
 
 
 class ImagesGenerator:
-    def __init__(self, model_path: pathlib.Path, seed: int, num_images: int) -> None:
-        tf.compat.v1.enable_eager_execution()
-        model = tf.compat.v2.saved_model.load(str(model_path))
+    def __init__(
+        self, model_path: pathlib.Path, seed: Optional[int], num_images: int
+    ) -> None:
+        self.g: StyleGAN
+        if tf.__version__.startswith("2."):
+            self.g = TF2Generator(model_path)
+        else:
+            self.g = TF1Generator(model_path)
         rnd = np.random.RandomState(seed)
-        z = rnd.randn(num_images, 512)
-        self.dlatents = model.signatures["mapping"](
-            latents=tf.convert_to_tensor(z, tf.float32)
-        )["dlatents"].numpy()
-        self.synthesis = model.signatures["synthesis"]
+        self.z = rnd.randn(num_images, 512)
 
     def generate(
         self, offset: Optional[np.ndarray] = None
     ) -> Generator[Tuple[np.ndarray, np.ndarray], None, None]:
-        for dlatents_in in self.dlatents:
+        dlatents = self.g.mapping(latents=self.z)
+        for dlatents_in in dlatents:
             inputs = np.array(dlatents_in)
             if offset is not None:
                 inputs += offset
-            images = self.synthesis(dlatents=tf.convert_to_tensor([inputs], tf.float32))
-            yield images["images"].numpy()[0], inputs
+            images = self.g.synthesis(inputs)
+            yield images[0], inputs
 
     def images(self, offset: Optional[np.ndarray] = None) -> List[np.ndarray]:
         return [image for image, _ in self.generate(offset)]
